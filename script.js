5fcdfcb5e2c2d769c0eae848238afc2c2432bb36
Gisel23 <giselpm@yahoo.com>
2023-09-14 19:52:56 -0300

    subiendo el register

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let bienvenida = alert("Te damos la bienvenida a nuestra página");
+let nombre = prompt("Queremos saber tu nombre");
+let mensaje = alert("Gracias por visitarnos y esperamos te guste nuestra porpuesta");
+
